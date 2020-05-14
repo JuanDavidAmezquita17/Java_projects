c0ec328d5a84a9021f497e0fd110ef350d6a2a34
Juan Amezquita <juan.amezquita@usantoto.edu.co>
2020-05-14 12:51:14 -0500

    14/05/2020
    P2T36

---
+++ CodeGym/src/Usta/sistemas/Main.java
@@ -0,0 +1,11 @@
+package Usta.sistemas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int c = 1; i <= 10; i++)
+                System.out.print(c * i+" ");
+        }
+    }
+}
